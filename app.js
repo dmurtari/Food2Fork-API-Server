f709691e4d32ea8c76e77e3e81bd2467c82e1a70
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-06 22:40:54 -0700

    Fixed incorrect responses

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ app.get('/', function(req, res) {
 app.get('/recipe/search/:text', function(req, res) {
   
   // These code snippets use an open-source library. http://unirest.io/nodejs
-  unirest.get("https://community-food2fork.p.mashape.com/search?key=b4e4e7719385dcbf54c699de0fe0a5ba")
+  unirest.get("https://community-food2fork.p.mashape.com/search?key=b4e4e7719385dcbf54c699de0fe0a5ba&q=" + req.params.text)
   .header("X-Mashape-Key", "LgPqtxyZeLmshtqZd4rQ5jHqBH3mp1lNoE4jsng5515D7TCs80")
   .header("Accept", "application/json")
-  .send({"q": req.params.text})
   .end(function (result) {
     console.log(result.status, result.headers, result.body);
     res.send(result)
@@ -24,10 +23,9 @@ app.get('/recipe/search/:text', function(req, res) {
 app.get('/recipe/get/:recipe', function(req, res) {
   
   // These code snippets use an open-source library. http://unirest.io/nodejs
-  unirest.get("https://community-food2fork.p.mashape.com/get?key=b4e4e7719385dcbf54c699de0fe0a5ba")
+  unirest.get("https://community-food2fork.p.mashape.com/get?key=b4e4e7719385dcbf54c699de0fe0a5ba&rId=" + req.params.recipe)
   .header("X-Mashape-Key", "LgPqtxyZeLmshtqZd4rQ5jHqBH3mp1lNoE4jsng5515D7TCs80")
   .header("Accept", "application/json")
-  .send({"rId": req.params.recipe})
   .end(function (result) {
     console.log(result.status, result.headers, result.body);
     res.send(result)
